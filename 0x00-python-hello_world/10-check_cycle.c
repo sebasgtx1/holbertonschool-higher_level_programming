124f3b2b71de1afca14cc0a8fca821c9673b3838
SebasGTX1 <sebas-0202@hotmail.es>
2022-05-02 12:27:55 -0500

    Uodating files

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: pointer to the singly linked list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{}
