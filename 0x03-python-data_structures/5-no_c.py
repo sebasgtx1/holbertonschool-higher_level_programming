5f7660e5b2fa1533bca5f08d7180965c344d181b
SebasGTX1 <sebas-0202@hotmail.es>
2022-05-09 12:43:07 -0500

    Revert "And for or"
    This reverts commit 280a03434776e73cb5d14688fc563c1108c847ae.

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     copy = ""
     for i in range(len(my_string)):
-        if my_string[i] != 'c' or my_string[i] != 'C':
+        if my_string[i] != 'c' and my_string[i] != 'C':
             cpy += my_string[i]
     return copy
