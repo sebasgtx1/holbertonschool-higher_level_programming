1aba39fa5c0a7207a52710afb9728c70db459471
SebasGTX1 <sebas-0202@hotmail.es>
2022-05-09 12:30:29 -0500

    Adding the 0 idx

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     cpy = my_list[:]
-    if idx > 0 or idx < len(my_list):
+    if 0 <= idx < len(my_list):
         cpy[idx] = element
     return cpy
