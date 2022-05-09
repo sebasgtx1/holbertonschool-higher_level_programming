99496936e771f36c9d4998ef76113278823a9713
SebasGTX1 <sebas-0202@hotmail.es>
2022-05-09 12:28:51 -0500

    Optimizing and fixing sintax

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    cpy = my_list.copy()
-    if idx < 0 or idx > len(my_list):
-        return cpy
-    else:
+    cpy = my_list[:]
+    if idx > 0 or idx < len(my_list):
         cpy[idx] = element
-        return cpy
+    return cpy
