758cc807c5027915abb3412bd1d0df7da8846213
SebasGTX1 <sebas-0202@hotmail.es>
2022-05-10 16:39:40 -0500

    Adv task 1 complete and test

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    avarage = 0
+    tuplee = 1
+    w = 0
+    for j, v in enumerate(my_list):
+        for i in range(len(v)):
+            tuplee *= v[i]
+        w += v[i]
+        avarage += tuplee
+        tuplee = 1
+    avarage /= w
+    return avarage
