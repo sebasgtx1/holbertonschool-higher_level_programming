6f20618d93f740723a63822397156437822c78e4
SebasGTX1 <sebas-0202@hotmail.es>
2022-05-10 16:20:01 -0500

    adding check if roman_string is a string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,8 @@
 def roman_to_int(roman_string):
     if not roman_string or roman_string is None:
         return 0
+    if not isinstance(roman_string, str):
+        return 0
     convert = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     decimal = 0
     n = 0
