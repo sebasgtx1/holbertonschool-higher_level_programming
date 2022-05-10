c52eace7a9ba1f727006e457fd429459778156c1
SebasGTX1 <sebas-0202@hotmail.es>
2022-05-10 16:17:12 -0500

    XIX case solved

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,12 +4,17 @@ def roman_to_int(roman_string):
         return 0
     convert = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     decimal = 0
+    n = 0
+    i = 1
     if len(roman_string) > 1:
-        for i, v in enumerate(roman_string):
-            if i == 0 or convert[roman_string[i - 1]] >= convert[v]:
-                decimal += convert[v]
-            else:
-                decimal -= convert[v]
+        while (i < len(roman_string)):
+                if convert[roman_string[i]] <= convert[roman_string[n]]:
+                    decimal += convert[roman_string[n]]
+                else:
+                    decimal -= convert[roman_string[n]]
+                n += 1
+                i += 1
+        decimal += convert[roman_string[n]]
     else:
         decimal += convert[roman_string]
     if decimal < 0:
