cba971f278d52bb5d8e86eaa788262a9b71aeaa2
SebasGTX1 <sebas-0202@hotmail.es>
2022-05-24 11:12:41 -0500

    Task 1 complete

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    """ Class that defines a Square with size"""
+    def __init__(self, size=0):
+        """ Method that defines and initialize the size
+        of the square
+
+        Args:
+            param1 (int): size of the square
+        """
+        self.__size = size
