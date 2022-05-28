4f82fe78fc95ae7d612f3a48991a2bc32673d1f7
SebasGTX1 <sebas-0202@hotmail.es>
2022-05-28 09:14:04 -0500

    Adding test case to the task 2 and Task 6 complete

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Module to test the max_integer Module
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class myTests(unittest.TestCase):
+    """ This class is for define the test cases for
+        the max integer funtion
+    """ 
+    def testBadList(self):
+        """Test case: passing a bad list """
+        with self.assertRaises(TypeError):
+                max_integer([2, 'Im not a number'])
+
+    def testEmptyList(self):
+        """Test case: passing an empty list """
+        self.assertEqual(max_integer([]), None)
+    
+    def testGoodList(self):
+        """Test case: passing a good list """
+        self.assertEqual(max_integer([1, 2, 3, 100]), 100)
+
+    def testGoodListNeg(self):
+        """Test case: passing a good list
+            with negative numbers
+        """
+        self.assertEqual(max_integer([-1, -2, -3]), -1)
+
+    def testBadList2(self):
+        """Test case: passing a bad list, the list
+            has a tuple"""
+        with self.assertRaises(TypeError):
+                max_integer([(2, 3), 3])
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
