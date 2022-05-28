268ae780378c7d781797a1dd25a1c30c7c1eea69
SebasGTX1 <sebas-0202@hotmail.es>
2022-05-28 14:40:54 -0500

    Adding test and depuring the code

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -13,24 +13,21 @@ def matrix_divided(matrix, div):
             a new matrix with the result
     """
     error = "matrix must be a matrix (list of lists) of integers/floats"
-    i = 0
-    j = 0
     result = []
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     elif div == 0:
         raise ZeroDivisionError("division by zero")
-    else:
-        if not all([isinstance(item, list) for item in matrix]):
+    if not matrix or not all([isinstance(item, list) for item in matrix]):
+        raise TypeError(error)
+    if len(matrix) == 0:
+        raise TypeError(error)
+    if not all([len(item) == len(matrix[0]) for item in matrix]):
+        raise TypeError("Each row of the matrix must have the same size")
+    for i in range(len(matrix)):
+        if not all([isinstance(item, (int, float)) for item in matrix[i]]):
             raise TypeError(error)
-        if len(matrix) == 0:
-            raise TypeError(error)
-        if not all([len(item) == len(matrix[0]) for item in matrix]):
-            raise TypeError("Each row of the matrix must have the same size")
-        for i in range(len(matrix)):
-            if not all([isinstance(item, (int, float)) for item in matrix[i]]):
-                raise TypeError(error)
-            else:
-                row = [round(element / div, 2) for element in matrix[i]]
-                result.append(row)
+        else:
+            row = [round(element / div, 2) for element in matrix[i]]
+            result.append(row)
     return result
