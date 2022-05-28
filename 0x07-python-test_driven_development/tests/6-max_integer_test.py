69a0b7eaef2063bc5ec4cd585127aaec1bac9f67
SebasGTX1 <sebas-0202@hotmail.es>
2022-05-28 09:21:07 -0500

    Notation and Pycodestyle fix

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-""" Module to test the max_integer Module
+"""Unittest for max_integer([..])
 """
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
-class myTests(unittest.TestCase):
+class TestMaxInteger(unittest.TestCase):
     """ This class is for define the test cases for
-        the max integer funtion
-    """ 
+    the max integer funtion
+    """
     def testBadList(self):
         """Test case: passing a bad list """
         with self.assertRaises(TypeError):
-                max_integer([2, 'Im not a number'])
+            max_integer([2, 'Im not a number'])
 
     def testEmptyList(self):
         """Test case: passing an empty list """
         self.assertEqual(max_integer([]), None)
-    
+
     def testGoodList(self):
         """Test case: passing a good list """
         self.assertEqual(max_integer([1, 2, 3, 100]), 100)
@@ -32,7 +32,8 @@ class myTests(unittest.TestCase):
         """Test case: passing a bad list, the list
             has a tuple"""
         with self.assertRaises(TypeError):
-                max_integer([(2, 3), 3])
+            max_integer([(2, 3), 3])
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
