56b7465a8e8a178423431fe2545f9620fbd30b76
SebasGTX1 <sebas-0202@hotmail.es>
2022-05-31 11:13:30 -0500

    Task 3 complete, Task 2 permissions

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
-""" 2. Area and Perimeter:
+""" 3. String representation:
     Write a class Rectangle that defines
-    a rectangle by: (based on 1-rectangle.py)
+    a rectangle by: (based on 2-rectangle.py)
 """
 
 
 class Rectangle:
     """ class Rectangle that defines a rectangle
     """
+    def __str__(self):
+        """ Method that creates a string with the
+        propeties of the rectangule
+            Return:
+                the rectangule in a printable form
+        """
+        rectangule = ""
+        if self.__width == 0 or self.__height == 0:
+            return rectangule
+        for i in range(self.__height):
+            rectangule += "#"*self.__width
+            if i < self.__height - 1:
+                rectangule += "\n"
+        return rectangule
+
     def __init__(self, width=0, height=0):
         """ Initial constructor to create width and height
         """
