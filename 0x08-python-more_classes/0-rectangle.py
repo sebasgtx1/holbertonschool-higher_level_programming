19ba869cc2d73a8e468eb1f71cabc21cbcb1735e
SebasGTX1 <sebas-0202@hotmail.es>
2022-05-31 10:26:04 -0500

    Task 0 complete

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" 0. Simple rectangle:
+    Write an empty class Rectangle that defines a rectangle
+"""
+
+
+class Rectangle:
+    """ empty class Rectangle that defines a rectangle
+    """
+    pass
