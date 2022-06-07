2f97ab531a42d34da0b3931197fed32c0a7ca5d7
SebasGTX1 <sebas-0202@hotmail.es>
2022-06-07 12:43:34 -0500

    Task 6 complete

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""  6. Improve Geometry:
+    based on 5-base_geometry.py
+"""
+
+
+class BaseGeometry:
+    """class BaseGeometry"""
+    def area(self):
+        """ method that raises an exception"""
+        raise Exception("area() is not implemented")
