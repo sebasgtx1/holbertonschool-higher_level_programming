358feb5135f083b80d62eecdab48e6a36c17a22a
SebasGTX1 <sebas-0202@hotmail.es>
2022-06-08 12:14:28 -0500

    Task 3 complete

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" 3. To JSON string """
+import json
+
+
+def to_json_string(my_obj):
+    """ function that returns the JSON representation of an object
+    """
+    return json.dumps(my_obj)
