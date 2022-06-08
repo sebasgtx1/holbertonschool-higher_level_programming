b5df17b6fbe3957c6a8e6cd6f62d4ec78b85840c
SebasGTX1 <sebas-0202@hotmail.es>
2022-06-08 11:43:19 -0500

    Pycodestyle fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,9 +3,8 @@
 
 
 def read_file(filename=""):
-    """ function that reads a text file 
+    """ function that reads a text file
         (UTF8) and prints it to stdout
     """
     with open(filename, encoding="utf-8") as MyFile:
         print(MyFile.read())
-
