cb81de2fe0c7e8c7096f45cbf974f5e3c4484541
SebasGTX1 <sebas-0202@hotmail.es>
2022-06-12 17:18:07 -0500

    Task 15 complete

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ 1. base class """
+import json
 
 
 class Base():
@@ -14,3 +15,11 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """ public method that returns the JSON representation of
+            a list of dictionaries
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return []
+        return json.dumps(list_dictionaries)
