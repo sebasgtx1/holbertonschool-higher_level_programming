85cfa7c1ca40c2d6dc30cfb73c709ee3d123c01c
SebasGTX1 <sebas-0202@hotmail.es>
2022-06-12 16:26:27 -0500

    Task 10 __str__ output correction, Task 11 complete

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,9 +9,20 @@ class Square(Rectangle):
         """ constructor method """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """ size getter """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ size setter """
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """public method that retrives the print and str calls"""
         value = "[Square] ({}) ".format(self.id)
         value += "{}/{} - ".format(self.x, self.y)
-        value += "{}/{}".format(self.width, self.height)
+        value += "{}".format(self.size)
         return value
