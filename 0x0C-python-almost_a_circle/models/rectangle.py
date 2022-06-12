d2381a9a2b4c14602f71e2f9a2074a63b0ba61ce
SebasGTX1 <sebas-0202@hotmail.es>
2022-06-12 14:34:24 -0500

    Task 1 complete

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+""" 2. first rectangle """
+from models.base import Base
+
+
+class Rectangle(Base):
+    """class Rectangle that inherits from Base"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Constructor method """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """ width getter """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ width setter """
+        self.__width = value
+
+    @property
+    def height(self):
+        """ height getter """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ height setter """
+        self.__hegit = value
+
+    @property
+    def x(self):
+        """ x getter """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ x setter """
+        self.__x = value
+
+    @property
+    def y(self):
+        """ y getter """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ y setter """
+        self.__y = value
