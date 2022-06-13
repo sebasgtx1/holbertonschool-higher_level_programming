159c309e19db8ccc69d4e1d93d566857cfb0d14e
SebasGTX1 <sebas-0202@hotmail.es>
2022-06-13 13:49:37 -0500

    Task 16 complete

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,10 +16,26 @@ class Base():
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
-        """ public method that returns the JSON representation of
+        """ static method that returns the JSON representation of
             a list of dictionaries
         """
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return []
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ public method that writes the JSON string
+            representation of list_objs to a file
+        """
+        filename = "{}.json".format(cls.__name__)
+        json_list = []
+
+        if list_objs is not None:
+            for i in range(len(list_objs)):
+                json_list.append(list_objs[i].to_dictionary())
+
+        with open(filename, mode='w', encoding='utf-8') as MyFile:
+            MyFile.write(cls.to_json_string(json_list))
