fa7dee520d7a1c5a476c3f83a8b62351710c4192
SebasGTX1 <sebas-0202@hotmail.es>
2022-06-14 13:27:59 -0500

    None Return fixed

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,7 +23,7 @@ class Base():
             a list of dictionaries
         """
         if list_dictionaries is None or len(list_dictionaries) == 0:
-            return []
+            return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
