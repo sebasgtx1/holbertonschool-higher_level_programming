70a1e78c2281be6c57e1558bbcee56c9889c7e7c
SebasGTX1 <sebas-0202@hotmail.es>
2022-06-14 13:31:15 -0500

    Adding edge case

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -56,7 +56,10 @@ class Base():
         """ public method that returns an instance with
             all attributes already set
         """
-        dummy = cls(1, 1)
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        else:
+            dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
 
