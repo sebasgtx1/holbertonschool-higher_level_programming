289dc74ac8846541af18052619394b19623ac9e1
SebasGTX1 <sebas-0202@hotmail.es>
2022-07-12 17:23:27 -0500

    Adding task 13 and task 15, both complete

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- Revomes records
+-- Query to remove records with score <= 5
+DELETE FROM second_table WHERE score <= 5;
---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,3 @@
+-- lists the number of records with the same score in the table second_table
+-- Query to lists the number of records with the same score in the table second_table
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score;
