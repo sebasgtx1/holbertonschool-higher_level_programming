ccf12797afbd227fd7dfc0043755b22d1f792e92
SebasGTX1 <sebas-0202@hotmail.es>
2022-07-13 12:10:01 -0500

    Adv task 3 complete

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,3 @@
+-- Displays the average temperature of the top 3 cities
+-- Query to display the top 3 average temperature by city ordered by temperature
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month = 7 OR month = 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
