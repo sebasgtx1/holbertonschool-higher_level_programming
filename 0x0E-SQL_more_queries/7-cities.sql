3ef9903617c8b3eb23cfc555fbcf35ee649c0e62
SebasGTX1 <sebas-0202@hotmail.es>
2022-07-13 21:30:47 -0500

    Task 6 Fix NOT NULL and Task 7 complete

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,5 @@
+-- Creates a Table
+-- Query to create a table cities
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
+(id INT UNIQUE AUTO_INCREMENT NOT NULL, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, 
+PRIMARY KEY (id), FOREIGN KEY (state_id) REFERENCES states(id));
