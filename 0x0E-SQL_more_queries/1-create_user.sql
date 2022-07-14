f293e2560f396d9b7495730244eafa58b6eaf0e3
SebasGTX1 <sebas-0202@hotmail.es>
2022-07-13 19:57:49 -0500

    Adding password identification

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Create User
 -- Query to create the server user_0d_1.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
