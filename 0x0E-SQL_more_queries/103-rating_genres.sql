3f6a1e03c53650e3118c2ef5eb3b00199eb40b7a
SebasGTX1 <sebas-0202@hotmail.es>
2022-07-14 20:52:24 -0500

    Adv task 3 complete

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,8 @@
+-- script that list series by rating
+-- Query to lists all the records by rating
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id 
+LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_show_genres.show_id
+GROUP BY tv_genres.name
+ORDER BY rating DESC;
