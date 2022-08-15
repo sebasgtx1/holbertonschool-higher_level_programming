30d043eabea715fc0ee73cb0e348b73c172210f1
SebasGTX1 <sebas-0202@hotmail.es>
2022-08-15 12:47:42 -0500

    Args index fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const args = process.argv;
-if (args.length === 1) {
+if (args.length === 3) {
   console.log('Argument found');
-} else if (args.length > 0) {
+} else if (args.length > 3) {
   console.log('Arguments found');
 } else {
   console.log('No argument');
