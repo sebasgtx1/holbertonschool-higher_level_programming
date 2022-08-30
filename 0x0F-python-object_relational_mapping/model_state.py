50151baa8455fe369c86ce6a150d899c2647b6cf
SebasGTX1 <sebas-0202@hotmail.es>
2022-08-30 16:57:36 -0500

    Fixing the MetaData Asignation

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,11 +2,11 @@
 """
 Contains State class that inherits from Base (declarative_base())
 """
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
+mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
-
 
 class State(Base):
     """
