83fa5db166a354394e946ac043fe05e9e8eb34ed
SebasGTX1 <sebas-0202@hotmail.es>
2022-08-30 16:13:27 -0500

    Task 4 complete - fixing JOIN stament

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
                            password=sys.argv[2], database=sys.argv[3])
     mycursor = mydb.cursor()
     q = "SELECT cities.id, cities.name, states.name FROM states\
-        LEFT JOIN cities ON states.id = cities.state_id ORDER BY cities.id ASC"
+        JOIN cities ON states.id = cities.state_id ORDER BY cities.id ASC"
     mycursor.execute(q)
     myresult = mycursor.fetchall()
     for row in myresult:
