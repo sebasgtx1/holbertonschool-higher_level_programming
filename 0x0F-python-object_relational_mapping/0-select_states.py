e80e13c96298fe9971530235f85d930958bea1ea
SebasGTX1 <sebas-0202@hotmail.es>
2022-08-30 14:53:30 -0500

    Task 0 complete

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" script that lists all states from the database hbtn_0e_0_usa
+"""
+if __name__ == "__main__":
+
+    import sys
+    import MySQLdb
+
+    mydb = MySQLdb.connect(host="localhost",
+                           port=3306, user=sys.argv[1],
+                           password=sys.argv[2],
+                           database=sys.argv[3])
+
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT * FROM states ORDER BY id ASC")
+    myresult = mycursor.fetchall()
+    for row in myresult:
+        print(row)
+
+    mycursor.close()
+    mydb.close()
