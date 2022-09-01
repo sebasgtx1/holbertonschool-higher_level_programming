3d27b13582938d7342edf29789ada8cd3840cd81
SebasGTX1 <sebas-0202@hotmail.es>
2022-09-01 14:04:58 -0500

    Fixing comment size and Task 3 complete

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a request to that URL, and displays the size of the body of the response
-# -s curl flag hide progress bar output when using shell scripts
-# -c wc flag print the byte counts
 curl -s "$1" | wc -c
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a GET request to the URL, and displays the body of the response
-# -L curl flag alows any type of redirections, so, always get a 200 OK status code (the request was successful)
 curl -sL "$1"
