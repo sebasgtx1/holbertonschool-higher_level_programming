cf8f21505301d21c1f36bec7b95504ad6332915b
SebasGTX1 <sebas-0202@hotmail.es>
2022-09-09 16:10:53 -0500

    [FIX]: Fixing general except anti-pattern

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     try:
         response = requests.get(sys.argv[1])
         print(response.headers['X-Request-Id'])
-    except:
+    except KeyError:
         print('None')
