9e66fe019ef0bfc6874fb9eb067242adbcbda0f1
SebasGTX1 <sebas-0202@hotmail.es>
2022-09-09 15:40:31 -0500

    [ADD]: Task 3 complete

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL,
+sends a request to the URL and displays
+the body of the response (decoded in utf-8).
+"""
+from urllib import request, error
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('utf-8'))
+    except error.HTTPError as e:
+        print('Error code: {}'.format(e.code))
