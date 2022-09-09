54a5eca4420a8519baa18f29e62a15d7787ac60f
SebasGTX1 <sebas-0202@hotmail.es>
2022-09-09 16:33:15 -0500

    [ADD]: Task 8 complete

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    q = sys.argv[1] if sys.argv[1] and sys.argv[1].isalpha() else ""
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    try:
+        r_json = r.json()
+        id_, name = file.get("id"), file.get("name")
+        print("[{}] {}".format(id_, name)) if r_json else print("No result")
+    except ValueError:
+        print('Not a valid JSON')
