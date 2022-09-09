807a6bd7766b1e1feabdc0e4e61e4c11f24d4569
SebasGTX1 <sebas-0202@hotmail.es>
2022-09-09 15:11:42 -0500

    [FIX]: Autoimport restriction

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL and
+"""
+Python script that takes in a URL, sends a request to the URL and
 displays the value of the X-Request-Id variable found in the header
 of the response.
 """
 import urllib.request
 import sys
 
-with urllib.request.urlopen(sys.argv[1]) as res:
-    print(res.headers.get('X-Request-Id'))
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as res:
+        print(res.headers.get('X-Request-Id'))
